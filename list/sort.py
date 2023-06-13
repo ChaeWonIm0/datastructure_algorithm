1b03bcf11544208c52d039700530d90471d7e5c3
dla9944 <dla9944@naver.com>
2023-06-13 17:13:05 +0900

    sort&twopointer solve

---
+++ list/sort.py
@@ -0,0 +1,14 @@
+def solution(nums, target):
+    nums.sort()
+    n = len(nums)
+    l, r = 0, n-1
+    while l < r:
+        if nums[l] + nums[r] < target:
+            l += 1
+        elif nums[l] + nums[r] > target:
+            r -= 1
+        elif nums[l] + nums[r] == target:
+            return True    
+    return False
+
+print(solution(nums=[1,5,2,6,1,7,3,8,15], target = 10))
