b2b3a23a833ddb6f563aa62d5ea45053b5692091
dla9944 <dla9944@naver.com>
2023-06-20 22:39:01 +0900

    DP prob

---
+++ Dynamic Programming/dp_test.py
@@ -0,0 +1,31 @@
+# O(n^2)도 가능
+# 문제에서 기본 제공
+from collections import deque
+class Solution:
+    def minCostClimbingStairs(self, cost: list[int]) -> int:
+        length = len(cost)
+        current_visited = 0
+        q = deque()
+        len_list = []
+        len_list2 = []
+        for v in range(length):
+            if v % 2 ==1:
+                len_list.append(v) # 짝수 리스트(0포함)
+        for v in range(length):
+            if v % 2 ==0:
+                len_list2.append(v) # 홀수 리스트
+        for i in cost:
+            q.append(i)
+        while q:
+            current_visited = q.popleft()
+            for i in len_list:
+                for j in len_list2:
+                    if cost[j] < cost[i]:
+                        current_visited += current_visited
+                    if cost[i] < cost[j]:
+                        current_visited += 0
+                    current_visited += current_visited
+            return current_visited
+S = Solution
+cost = [10, 15, 20]
+S.minCostClimbingStairs(S, cost)
--- Dynamic Programming/top-down.py
+++ Dynamic Programming/top-down.py
@@ -1,7 +1,9 @@
 memo = {}
 def fibo(n):
+    # 가장 아래쪽까지 내려가서 n이 3일때 1+2부터 시작
     if n == 1 or n ==2:
         return 1
+    # n이 4일 경우 3+2부터 시작
     if n not in memo:
         memo[n] = fibo(n-1) + fibo(n-2)
     return memo[n]
